bbbf2dfbd03dc620f67aba855e5115c2c466ffb0
Skoopyy <104071935+Skoopyy@users.noreply.github.com>
2022-06-03 15:21:45 +0700

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -1,12 +1,13 @@
 @echo off
+title Fixing Microshaft software
 goto fix
 
 :fix
 cd C:\Windows\System32
-reg export HKEY_CLASSES_ROOT\ms-msdt C:\ms-greatatsecuringsoftware.reg
+reg export HKEY_CLASSES_ROOT\ms-msdt C:\bk.reg
 reg delete HKEY_CLASSES_ROOT\ms-msdt /f
 goto end
 
 :end
-echo Thanks Microsoft for making secure software!
+echo Thanks Microsoft for making shit software!
 pause
